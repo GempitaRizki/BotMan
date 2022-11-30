944917d678f41417ec210936fd1316ed32a3f285
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-11-30 16:20:38 +0700

    first greeting

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -2,31 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use BotMan\BotMan\Messages\Incoming\Answer;
 use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Incoming\Answer;
+use Illuminate\Http\Request;
 
 class BotManController extends Controller
 {
-    public function handle ()
+
+    public function handle()
     {
         $botman = app('botman');
 
-        $botman->hears('{$message}', function ($botman, $message){
+        $botman->hears('{message}',function($botman,$message){
 
-            if($message === 'hi'){
+            if ($message == 'Hallo') {
                 $this->askName($botman);
-            } else{
-                $botman->replay("write 'hi' for testing...");
+            }else{
+                $botman->reply("Katakan 'Hallo' untuk melanjutkan percakapan");
             }
+
         });
+
         $botman->listen();
     }
 
     public function askName($botman)
     {
-        $botman->ask("Hallo! Siapa namamu ?", function(Answer $answer){
+        $botman->ask("Hallo! Siapa namamu?",function(Answer $answer){
             $name = $answer->getText();
+
+            $this->say('Senang berkenalan denganmu '.$name);
         });
     }
 }
